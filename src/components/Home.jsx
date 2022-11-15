a878a320fc6364ec9d16a5c947cbecdc86987a22
Ahmed SAHNOUN <asahnoun@dom.tti>
2022-11-15 18:24:25 +0100

    project map

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,8 +10,8 @@ const Home = () => {
 				viewport={{ once: true }}
 				transition={{ duration: 0.8 }}
 				variants={{
-					visible: { opacity: 1, x: 0 },
-					hidden: { opacity: 0, x: -300 }
+					visible: { opacity: 1, y: 0 },
+					hidden: { opacity: 0, y: 50 }
 				}}
 			>
 				<section className="home" id="home">
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,48 @@
+import pic from "../Assets/portfolio1.jpg"
+
+const project = (details) => {
+	return (
+		<div className="row">
+			<a
+				style={{ color: "inherit" }}
+				href={details.link}
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<img src={details.pic} alt="" />
+				<div className="layer">
+					<h5>{details.title}</h5>
+					<p>{details.description}</p>
+					<i className='bx bx-link-external'></i>
+				</div>
+			</a>
+		</div>
+	)
+}
+
 const Projects = () => {
+
+	const list = [
+		{
+			title: "a",
+			description: "b",
+			pic: pic,
+			link: ""
+		},
+		{
+			title: "a",
+			description: "b",
+			pic: pic,
+			link: ""
+		},
+		{
+			title: "a",
+			description: "b",
+			pic: pic,
+			link: ""
+		}
+	]
+
 	return (
 		<>
 			<section className="portfolio" id="portfolio">
@@ -7,55 +51,7 @@ const Projects = () => {
 					<h2><span>Latest</span> Projects</h2>
 				</div>
 				<div className="portfolio-content">
-					<div className="row">
-						<img src="img/rhuma.jpg" height="400" alt=""/>
-							<div className="layer">
-								<h5>Rhuma Kids</h5>
-								<p>Mobile application for diagnostic and assistance in the management of patients with spondyloarthritis
-									for the benefit of the Kassab Manouba institute Tunis, Tunisia.</p>
-								<a href="https://github.com/omar-chaabouni/diagnostique-rhumatologie" target="_blank" rel="noopener noreferrer"><i
-									className='bx bx-link-external'></i></a>
-							</div>
-					</div>
-					<div className="row">
-						<img src="img/cows.png" height="400" alt=""/>
-							<div className="layer">
-								<h5>Cows & Bulls</h5>
-								<p>A reflexion game that I made individually with Flutter and Firebase. Available on Play store via this
-									link : This game was made for my own educational purposes. I wanted to do a project so I can put my
-									knowladge into test and use. But thankfully it turned out to be a great game.</p>
-								<a href="https://github.com/omar-chaabouni/cows-and-bulls" target="_blank" rel="noopener noreferrer"><i
-									className='bx bx-link-external'></i></a>
-							</div>
-					</div>
-				</div>
-			</section>
-			<div className="main-text">
-				<h2><span>Latest</span> Achievements</h2>
-			</div>
-			<section className="portfolio">
-				<div className="portfolio-content">
-					<div className="row">
-						<img src="img/eysey.jpg" alt=""/>
-							<div className="layer">
-								<h5>Eyesy</h5>
-								<p>Smart hat to help visually impaired people navigate their path.
-									Project winner of the first edition of Makeathon inter ODC Clubs organized jointly by Orange Tunisie
-									and GIZ Tunisie.</p>
-								<a href="https://github.com/omar-chaabouni/eyesy" target="_blank" rel="noopener noreferrer"><i className='bx bx-link-external'></i></a>
-							</div>
-					</div>
-					<div className="row">
-						<img src={"../Assets/covid.jpg"} height="380" alt=""/>
-							<div className="layer">
-								<h5>Rendez-vous-vaccin</h5>
-								<p>Project in the crisis management theme: result of participation in ENSI's TuniHack 6.0 hackaton.
-									Second place winning project.
-									This project idea was adopted by the Tunisian government which is a great honor to us.</p>
-								<a href="https://github.com/omar-chaabouni/Rendez-vous-vaccin" target="_blank" rel="noopener noreferrer"><i
-									className='bx bx-link-external'></i></a>
-							</div>
-					</div>
+					{list.map((x) => project(x))}
 				</div>
 			</section>
 		</>
