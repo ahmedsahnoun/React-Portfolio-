fbefe53dcbd8437bca1565581c2456a1cb602f67
Ahmed SAHNOUN <asahnoun@dom.tti>
2022-12-20 12:40:49 +0100

    spring animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import Resume from "../Assets/Resume.pdf"
 const reveal = (letter, index, col, delay) => {
 	return (
 		<motion.span
-			style={{ color: col }}
+			style={{ color: col, display: "inline-block", minWidth: "1rem" }}
 			key={index}
 			initial="hidden"
 			whileInView="visible"
@@ -12,7 +12,7 @@ const reveal = (letter, index, col, delay) => {
 			transition={{ duration: 0.2, delay: delay + 0.08 * index }}
 			variants={{
 				visible: { opacity: 1, y: 0 },
-				hidden: { opacity: 0, y: -40 }
+				hidden: { opacity: 0, y: -20 }
 			}}
 		>
 			{letter}
@@ -43,9 +43,10 @@ const Home = () => {
 						<span className="two">I am</span>
 					</div>
 					<h1>Ahmed SAHNOUN</h1>
-					<h3 className="current-status"> {"Computer Science".split("").map((letter, index) => reveal(letter, index, "white", 0.2))}
+					<h3 className="current-status"> 
+						{"Computer Science".split("").map((letter, index) => reveal(letter, index, "white", 0.2))}
 						<br />
-						<span>{"Engineer".split("").map((letter, index) => reveal(letter, index, null, 1.6))}</span>
+						{"Engineer".split("").map((letter, index) => reveal(letter, index, null, 1.6))}
 					</h3>
 					<div className="button">
 						<a href={Resume}
